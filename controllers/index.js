a2d117ef1956f90878facab8ce069a183a350d1a
DonNguyen808 <DonNguyen808@gmail.com>
2022-10-25 20:16:36 -0400

    updated controllers

--- controllers/index.js
+++ controllers/index.js
@@ -1,1 +1,15 @@
+const { model } = require('mongoose')
 const schemas = require('../models/schemas.js')
+
+model.exports = {
+    getHome: async (req, res) => {
+        let menu = schemas.menu
+        let sesh = req.session
+
+        let menuResult = await menu.find({})
+        .then( (menuData)  => {
+            res.render('index', {title: 'Menu Tracker', data: menuData, search: '', loggedIn: sesh.loggedIn})
+        }
+    },
+    getSearch:
+}
