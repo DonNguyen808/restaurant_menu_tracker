11ba4b6018dc411df31a3f924486cf12698a6b07
DonNguyen808 <DonNguyen808@gmail.com>
2022-11-01 22:08:01 -0400

    fully completed index and login controllers

--- controllers/login.js
+++ controllers/login.js
@@ -40,7 +40,8 @@ exports.PostLogin = async (req, res) => {
             if (data) {
                 // bcrypt has a built in method that compares hashmap pass
                 let passResult = await bcrypt.compare(pass, data.pwd) 
-                .then((isMatch) => {
+                // whatever comes back from the bcrypt compare maybe true or false is put into the isMatch
+                .then((isMatch) => {  // hover over isMatch says its boolean
                     if (isMatch) {
                         sesh.loggedIn = true
                         loginSuccess = true
@@ -55,4 +56,32 @@ exports.PostLogin = async (req, res) => {
         res.render('login', {title: 'Login', loggedIn: false, error: 'Invalid Login!'})
     }
 
+
+    exports.PostSignup = async (req, res) => {
+        let email = req.body.emailInput
+        let pass = req.body.pwdInput
+
+        if (email !=- '' && pass !== '') {
+            let users = schemas.users
+            let qry = {email: email}
+
+            let userSearch = await users.findOne(qry)
+            .then(async(data) => {
+                if (!data) {
+                    let saltRounds = 10
+                    let passSalt = await bcrypt.genSalt(saltRounds, async(err, salt) => {
+                        let passHash = await bcrypt.hash(pass, salt, async(err, hash) => {
+                            let acct = {email: email, pwd: hash, level: 'admin'}
+                            let newUser = new schemas.users(acct)
+                            let saveUser = await newUser.save()
+                        }
+                    })
+                }
+            })
+
+            res.render('login', {title: 'Login', loggedIn: false, error: 'Please login with your new account'})
+        }else {
+            res.render('new-actt', {title: 'New Account', loggedIn: false, error: 'All fields are required. Please check and try again.'})
+        }
+        }
 }
